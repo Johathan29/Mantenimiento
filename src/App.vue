18310786b460f9c05d8e03cbd9095c80fbff918e
Johathan29 <84540634+Johathan29@users.noreply.github.com>
2023-11-18 06:09:47 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -206,7 +206,7 @@ const Delete = (index) => {
 <template>
   <HelloWorld />
   <section>
-    <button @click="" class="btn btn btn-success">Favorito</button>
+  
     <div class="container row">
       <ul class="row row-cols-1 row-cols-md-2 g-4">
         <li
