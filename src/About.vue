87adcb667c2cdfe2f884d31b2d3b2c967ea9a195
Johathan29 <84540634+Johathan29@users.noreply.github.com>
2024-03-22 11:20:07 -0400

    Update About.vue

--- src/About.vue
+++ src/About.vue
@@ -4,8 +4,9 @@ const users = ref(null);
 const capturar = ref();
 const itemUser = ref([]);
 const grupoItem = ref([]);
+  //https://jsonplaceholder.typicode.com/users
 onMounted(async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const response = await fetch('https://binx.hacienda.gob.do/Reports/powerbi/PUBLICACION/TRANSPARENCIA/DASHBOARDS/PTF_INDICADORES_PRESUPUESTO?rs:Embed=true');
   users.value = await response.json();
 });
 
